eef670396a837ca76a3cc0e822206f1a8b874e65
Adrian <aracosta2810@gmail.com>
2023-06-08 16:15:13 -0400

    Commit talloso

--- src/app/controllers/auth.controller.js
+++ src/app/controllers/auth.controller.js
@@ -15,19 +15,18 @@ const loginUser = async (req, res) => {
 
   try {
     const user = await User_Model.findOne({ where: { email } });
-    if (!user) throw new Error('This email does not exist');
+    if (!user) throw new Error('Este correo no existe');
 
     if (!(await bcryptjs.compare(password, user.password)))
-      throw new Error('Incorrect password');
+      throw new Error('Contraseña incorrecta');
 
     req.login(user, function (err) {
       if (err) throw new Error('Error al crear sesión');
       return res.redirect('/');
     });
   } catch (error) {
-    return res.json({ msf: error.message })
     req.flash('messages', [{ msg: error.message }]);
-    return res.redirect('/auth/login');
+    return res.redirect('/');
   }
 }
 
@@ -38,8 +37,48 @@ const logOut = (req, res) => {
   });
 };
 
+const getProfile = (req, res) => res.render('pages/Profile', { layout: false, user: req.user, messages: req.flash().messages });
+
+const saveProfileChanges = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    req.flash("messages", errors.array());
+    return res.redirect("/auth/Profile");
+  }
+
+  const { name, email, password, confirmPassword, currentPassword } = req.body;
+  const userId = req.user.id;
+
+  try {
+    const user = await User_Model.findOne({ where: { id: userId } });
+    if (!user) throw new Error('Usuario no encontrado');
+
+    if (currentPassword && !(await bcryptjs.compare(currentPassword, user.password)))
+      throw new Error('Contraseña actual incorrecta');
+
+    if (password && confirmPassword && password !== confirmPassword)
+      throw new Error('Las contraseñas no coinciden');
+
+    const updatedUser = {
+      name,
+      email,
+      password: password ? await bcryptjs.hash(password, 10) : user.password
+    };
+
+    await User_Model.update(updatedUser, { where: { id: userId } });
+
+    req.flash('messages', [{ msg: 'Los cambios han sido guardados correctamente' }]);
+    return res.redirect('/auth/Profile');
+  } catch (error) {
+    req.flash('messages', [{ msg: error.message }]);
+    return res.redirect('/auth/Profile');
+  }
+};
+
 export {
   loginForm,
   loginUser,
-  logOut
+  logOut,
+  getProfile,
+  saveProfileChanges
 };
--- src/app/routes/auth.routes.js
+++ src/app/routes/auth.routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 const router = Router()
 import { body } from "express-validator";
-import { loginForm, loginUser,logOut } from '../controllers/auth.controller.js';
+import { getProfile, loginForm, loginUser, logOut,saveProfileChanges } from '../controllers/auth.controller.js';
+import { userVerification } from '../middlewares/userVerification.js';
 
 router.get('/login', loginForm);
-router.post('/login',[
-    body('email',"Ingrese un email").trim().isEmail().normalizeEmail(),
+router.post('/login', [
+    body('email', "Ingrese un email").trim().isEmail().normalizeEmail(),
 ], loginUser);
 router.get("/logout", logOut);
+// Profile
+router.get('/profile', userVerification, getProfile)
+router.post('/save-profile-changes', userVerification, saveProfileChanges)
 
 export default router;
